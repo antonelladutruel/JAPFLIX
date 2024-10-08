8fa6d8f0521aad0bd80c309c9f4dcc65b32dfb25
antonelladutruel <antonelladutruel@gmail.com>
2024-10-08 15:46:51 -0300

    JapFlix Changes

--- js/script.js
+++ js/script.js
@@ -2,26 +2,100 @@ const input = document.getElementById("inputBuscar");
 const btn = document.getElementById("btnBuscar");
 const container = document.getElementById("lista");
 const url = "https://japceibal.github.io/japflix_api/movies-data.json";
-const titles= [];
-const overviews = [];
-const calification = [];
+const movies= [];
 
 
 fetch(url)
 .then(res => res.json())
 .then(data => data.forEach((product) => {
-titles.push(product.title);
-overviews.push(product.overview);
-calification.push(product.vote_average);
-
+    movies.push({
+        title: product.title,
+        tagline: product.tagline,
+        calification: product.vote_average,
+        overview: product.overview,
+        genres: product.genres,
+        year: product.release_date.split('-')[0],
+            runtime: product.runtime,
+            budget: product.budget,
+            revenue: product.revenue
+    });
 }))
-.catch("error", console.error);
+.catch(error => console.error("error",error));
 
+
+function generateStars(score) {
+    let stars = "";
+    for (let i = 1; i <= 5; i++) {
+        if (i <= score) {
+            stars += `<span class="fa fa-star checked"></span>`;
+        } else {
+            stars += `<span class="fa fa-star"></span>`;
+        }
+    }
+    return stars;
+}
 
 
 btn.addEventListener("click", filterMovie);
 
-function filterMovie(){
- const inputValue = input.value.toLowerCase();
- container.innerHTML = "";
-}
+function filterMovie() {
+    const inputValue = input.value.toLowerCase();
+    container.innerHTML = "";
+
+    movies.forEach((movie) => {
+        if (
+            movie.title.toLowerCase().includes(inputValue) || 
+            movie.tagline.toLowerCase().includes(inputValue)
+        ) {
+            const stars = generateStars(Math.round(movie.calification/2));
+            container.innerHTML += `
+<div class="container mt-6">
+  <div class="row justify-content-center">
+    <div class="col-md-11">
+      <div class="card mb-3 movie-card" data-overview="${movie.overview}" data-genres='${JSON.stringify(movie.genres)}'
+      data-year="${movie.year}" 
+           data-runtime="${movie.runtime}" 
+           data-budget="${movie.budget}" 
+           data-revenue="${movie.revenue}">>
+        <div class="card-body d-flex justify-content-between align-items-center" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+          <div >
+            <h5 class="card-title text-white fw-bold">${movie.title}</h5>  
+            <p class="card-text text-muted fst-italic"> ${movie.tagline}</p>
+          </div>
+                      <div class="stars">${stars}</div>
+         </div>
+       </div>
+     </div>
+   </div>
+</div>
+            `;
+        }
+    });
+
+
+     const movieCards = document.querySelectorAll('.movie-card');
+     movieCards.forEach(card => {
+         card.addEventListener('click', () => {
+             const overview = card.getAttribute('data-overview');
+             const genresArray = JSON.parse(card.getAttribute('data-genres'));
+             const genreNames = genresArray.map(genre => genre.name).join(', ');
+             const year = card.getAttribute('data-year');
+             const runtime = card.getAttribute('data-runtime');
+             const budget = card.getAttribute('data-budget');
+             const revenue = card.getAttribute('data-revenue');
+
+             const offcanvasBody = document.querySelector('.offcanvas-body');
+             offcanvasBody.innerHTML = `<p>${overview}</p>
+               <hr class="text-muted">
+
+             <p class="text-muted">${genreNames}</p>
+             `;
+
+             const dropdownItems = document.querySelectorAll('.dropdown-item');
+             dropdownItems[0].textContent = `Year: ${year}`;
+             dropdownItems[1].textContent = `Runtime: ${runtime} min`;
+             dropdownItems[2].textContent = `Budget: $${budget.toLocaleString()}`;
+             dropdownItems[3].textContent = `Revenue: $${revenue.toLocaleString()}`;
+         });
+     });
+ }
